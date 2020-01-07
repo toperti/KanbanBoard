5a9f3afaea964be506bdc2b145cd682f763cc3a6
Edwin G <edwgeo@gmail.com>
2020-01-07 12:44:06 -0800

    Editing Tasks Functionality added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	task = db.Column(db.String(150), unique = True, nullable = False)
+	category = db.Column(db.String, nullable = False)
 
 	def __repr__(self):
 		return '<Todo %r>' % self.task
@@ -15,6 +16,7 @@ class Todo(db.Model):
 class Inpro(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	task = db.Column(db.String(150), unique = True, nullable = False)
+	category = db.Column(db.String, nullable = False)
 
 	def __repr__(self):
 		return '<Inpro %r>' % self.task
@@ -22,6 +24,7 @@ class Inpro(db.Model):
 class Completed(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	task = db.Column(db.String(150), unique = True, nullable = False)
+	category = db.Column(db.String, nullable = False)
 
 	def __repr__(self):
 		return '<Completed %r>' % self.task
@@ -33,17 +36,19 @@ def index():
 		task = request.form['content']
 		category = request.form['category']
 		if category == "Todo":
-			new_task = Todo(task = task)
+			new_task = Todo(task = task, category = category)
 			db.session.add(new_task)
 			db.session.commit()
 			return redirect('/')
+
 		elif category == "Inpro":
-			new_task = Inpro(task = task)
+			new_task = Inpro(task = task, category = category)
 			db.session.add(new_task)
 			db.session.commit()
 			return redirect('/')
+
 		else:
-			new_task = Completed(task = task)
+			new_task = Completed(task = task, category = category)
 			db.session.add(new_task)
 			db.session.commit()
 			return redirect('/')
@@ -53,3 +58,49 @@ def index():
 		inpro = Inpro.query.all()
 		completed = Completed.query.all()
 		return render_template('index.html', todo = todo, inpro = inpro, completed = completed)
+
+@app.route('/edit/<string:category>/<int:id>', methods = ['GET', 'POST'])
+def edit(category, id):
+	if category == 'Todo':
+		task = Todo.query.get_or_404(id)
+	elif category == 'Inpro':
+		task = Inpro.query.get_or_404(id)
+	else:
+		task = Completed.query.get_or_404(id)
+
+	if request.method == 'POST':
+		if request.form['action'] == "Save Edits":
+			content = request.form['content']
+
+			post_category = request.form['category']
+			if category != post_category:
+				if post_category == "Todo":
+					db.session.delete(task)
+					new_task = Todo(task = content, category = post_category)
+					db.session.add(new_task)
+					db.session.commit()
+					return redirect('/')
+
+				elif post_category == "Inpro":
+					db.session.delete(task)
+					new_task = Inpro(task = content, category = post_category)
+					db.session.add(new_task)
+					db.session.commit()
+					return redirect('/')
+
+				else:
+					db.session.delete(task)
+					new_task = Completed(task = content, category = post_category)
+					db.session.add(new_task)
+					db.session.commit()
+					return redirect('/')
+			else:
+				task.content = content
+		else:
+			db.session.delete(task)
+			db.session.commit()
+			return redirect('/')
+
+	else:
+		return render_template('edit.html', task = task)
+
